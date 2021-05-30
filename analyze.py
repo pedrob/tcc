0d8daacfbcf0a350f02cc2c7251cee4fd9ce3615
Pedro Costa <pedrobeethoven8@gmail.com>
2021-05-29 21:14:22 -0300

    modify analysis algorithm

--- analyze.py
+++ analyze.py
@@ -1,38 +1,70 @@
-from nltk import word_tokenize
-import nltk
-import re
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import svm
-from sklearn import metrics
-from sklearn.model_selection import cross_val_predict
-from nltk.tokenize import TweetTokenizer
-import spacy
+# import spacy
+import json
+
+# def orderAsc(pessoa):
+#     pessoa['mencoes'].reverse()
+#     pessoa['datas'].reverse()
+#     return pessoa
+
+def converter_data(data):
+    nova_data = data.split("-")
+    nova_data.pop()
+    nova_data.reverse()
+    nova_data = "/".join(nova_data)
+    return nova_data
+
+def adicionar_valor_analise_de_sentimentos(neutro, pos, neg, res):
+    res['mencoesNeutras'].append(neutro)
+    res['mencoesPositivas'].append(pos)
+    res['mencoesNegativas'].append(neg)
 
 df = pd.read_csv('tweets.csv', encoding='utf-8')
-print(df.text.count())
 df.drop_duplicates(['text'], inplace=True)
-print(df.text.count())
-nlp = spacy.load('pt_core_news_sm')
-count = 10
-for row in df.text:
-    count += 1
-    texto = nlp(row)
-    for entidade in texto.ents:
-        print(entidade.text, entidade.label_)
-    if count == 2:
-        break
-# Preciso filtrar os tweets que só possuem entidade nomeada (nome do candidato)
-# e a o token presidente 2022
+index = df.index
+total = len(index)
 
-# Após isso, preciso calcular os nomes mais frequentes, depois filtrar os
-# tweets que possuem esse nome
+# modelo menor
+# nlp = spacy.load('pt_core_news_sm')
+# nlp = spacy.load('pt_core_news_lg')
 
-# depois preciso classificar cada um desses tweets filtrados com uma analise 
-# de sentimentos
 
-# Concluo com o candidato com mais referencias positivas e a tendencia ou nao de crescimento
+resposta = {
+    'termo': 'Lula Presidente 2022',
+    'mencoes': [],
+    'mencoesPositivas': [],
+    'mencoesNegativas': [],
+    'mencoesNeutras': [],
+    'datas': [],
+    "total": total
+}
 
-# Graficos: nuvem de palavras, barra, linha por tempo (para ilustrar a tendencia)
+for conteudo_texto, data, mencoes_neutras, polaridade in zip(df.text, df.date, df.neu_w, df.polarity):
+    data_ref = converter_data(data)
+    if data_ref in resposta['datas']:
+        if mencoes_neutras == 1:
+            resposta['mencoesNeutras'][-1] += 1
+        else:
+            if polaridade > 0:
+                resposta['mencoesPositivas'][-1] += 1
+            else:
+                resposta['mencoesNegativas'][-1] += 1
+        resposta['mencoes'][-1] += 1
+    else:
+        if mencoes_neutras == 1:
+            adicionar_valor_analise_de_sentimentos(1, 0, 0, resposta)
+        else:
+            if polaridade > 0:
+                adicionar_valor_analise_de_sentimentos(0, 1, 0, resposta)
+            else:
+                adicionar_valor_analise_de_sentimentos(0, 0, 1, resposta)
+        resposta['mencoes'].append(1)
+        resposta['datas'].append(data_ref)
+
+resposta['mencoes'].reverse()
+resposta['mencoesNeutras'].reverse()
+resposta['mencoesPositivas'].reverse()
+resposta['mencoesNegativas'].reverse()
+resposta['datas'].reverse()
+print(json.dumps(resposta))
 
--- get_data.py
+++ get_data.py
@@ -1,12 +1,14 @@
 from bsi_sentiment.twitter import search_tweets_sn
 
+# parametrizar os valores
 tweets = search_tweets_sn(
-  q="Presidente 2022",
+  q="Lula Presidente 2022",
   since="2019-01-01",
-  until="2021-03-01",
+  until="2021-05-01",
   near="Brasil",
-  lang="pt-br"
+  lang="pt-br",
+  max_tweets=10000
 )
 
-# tweets.get_sentiment(method="vader")
-tweets.to_csv("./tweets.csv")
+tweets.get_sentiment(method="vader")
+tweets.to_csv("./tweets-new-vader.csv")
